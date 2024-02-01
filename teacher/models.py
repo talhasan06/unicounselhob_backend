3b7c0b46104bbe9ffa7270811cbdcb5c2efd7142
Tamim Al Hasan <talhasan06@gmail.com>
2024-02-01 15:10:09 +0600

    teacher full name

--- teacher/models.py
+++ teacher/models.py
@@ -16,5 +16,8 @@ class Teacher(models.Model):
     available_time = models.ManyToManyField(AvailableTime)
     mobile_no = models.CharField(max_length=12)
 
+    def full_name(self):
+        return self.user.get_full_name()
+
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
